0f09930983d892413708050c0403b36f16f00b72
min <minhsiungw@gmail.com>
2024-05-17 11:44:58 +0800

    add carousel component 2

---
+++ packages/carousel/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ["eslint:recommended", "plugin:react-hooks/recommended"],
+  ignorePatterns: [".eslintrc.cjs"],
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
--- packages/carousel/src/index.js
+++ packages/carousel/src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Carousel from "./carousel";
+import Carousel1 from "./carousel1";
+import Carousel2 from "./carousel2";
 import "./style.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 const App = () => (
-  <div>
-    <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-      Carousel
-    </h1>
-    <Carousel />
-  </div>
+  <>
+    {/* <div>
+      <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Carousel1
+      </h1>
+      <Carousel1 />
+    </div> */}
+    <div>
+      <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Carousel2
+      </h1>
+      <Carousel2 />
+    </div>
+  </>
 );
 root.render(<App />);
